e69205a03ed7674cfe28924c52f2722387c3ed79
Andre <andy_garlop4@hotmail.com>
2025-06-08 16:39:56 -0600

    Cambio de color segun la temperatura

--- src/components/WetaherInfo.tsx
+++ src/components/WetaherInfo.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { useWeatherStore } from '../store/store'
 
 export default function WetaherInfo() {
@@ -8,15 +9,24 @@ export default function WetaherInfo() {
         return parseInt((temp - kelvin).toString())
     }
 
+
+
     return (
-        <div className='flex flex-col justify-center items-center space-y-4 p-5 m-2 glass mt-10 text-white text-shadow-md'>
+        <div className={clsx(
+            formatTemp(weatherData.main.temp) < 10 && 'glass-extreme-cold',
+            formatTemp(weatherData.main.temp) >= 10 && formatTemp(weatherData.main.temp) <= 17 && 'glass-cold',
+            formatTemp(weatherData.main.temp) > 17 && formatTemp(weatherData.main.temp) <= 24 && 'glass',
+            formatTemp(weatherData.main.temp) >= 25 && formatTemp(weatherData.main.temp) <= 30 && 'glass-hot',
+            formatTemp(weatherData.main.temp) > 30 && 'glass-extreme-hot',
+            'flex flex-col justify-center items-center space-y-4 p-5 m-2 mt-10 text-white text-shadow-md'
+        )}>
             <h2 className='text-center text-4xl'>{weatherData.name}</h2>
             <p className='text-6xl font-black'>{formatTemp(weatherData.main.temp)}°C</p>
 
             <div className='flex flex-col lg:flex-row justify-around items-center w-full '>
-                <p>Min: {formatTemp(weatherData.main.temp_min)}</p>
-                <p>Max: {formatTemp(weatherData.main.temp_max)}</p>
-                <p>Sensacion: {formatTemp(weatherData.main.feels_like)}</p>
+                <p>Min: {formatTemp(weatherData.main.temp_min)}°C</p>
+                <p>Max: {formatTemp(weatherData.main.temp_max)}°C</p>
+                <p>Sensacion: {formatTemp(weatherData.main.feels_like)}°C</p>
                 <p>Humedad: {weatherData.main.humidity}%</p>
             </div>
         </div>
